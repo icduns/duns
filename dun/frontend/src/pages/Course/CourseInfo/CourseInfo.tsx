7cfff7ac8731e11c066cdb0e8946ac3d90666048
Vitaly Lipko <v.lipko@bacup.ru>
2022-09-05 07:37:46 +0000

    feat(front): implement course page

--- dun/frontend/src/pages/Course/CourseInfo/CourseInfo.tsx
+++ dun/frontend/src/pages/Course/CourseInfo/CourseInfo.tsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
+import { Col, Row, Space, Typography } from 'antd';
+import { useTranslation } from 'react-i18next';
 import { Course } from '~/api';
+
+const { Title, Paragraph } = Typography;
 
 export type CourseInfoProps = {
   course: Course;
-  className?: string;
 };
 
-export function CourseInfo({ course, className }: CourseInfoProps) {
-  return <div className={className}>{course.description}</div>;
+function CourseInfoTile(props: PropsWithChildren<{ title: string }>) {
+  const { title, children } = props;
+
+  return (
+    <Space direction="vertical">
+      <Title level={5}>{title}</Title>
+      {children}
+    </Space>
+  );
+}
+
+export function CourseInfo({ course }: CourseInfoProps) {
+  const { t } = useTranslation();
+
+  return (
+    <Col span={24}>
+      <Row gutter={[16, 0]}>
+        <Col span={10}>
+          <CourseInfoTile title={t('courses.description')}>
+            <Paragraph type="secondary">{course.description}</Paragraph>
+          </CourseInfoTile>
+        </Col>
+        <Col>
+          <Row gutter={[0, 16]}>
+            <Col span={24}>
+              <CourseInfoTile title={t('courses.category')}>
+                Test
+              </CourseInfoTile>
+            </Col>
+            <Col span={24}>
+              <CourseInfoTile title={t('courses.level')}>Test</CourseInfoTile>
+            </Col>
+            <Col span={24}>
+              <CourseInfoTile title={t('courses.image')}>Test</CourseInfoTile>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Col>
+  );
 }
--- dun/frontend/src/pages/Course/CourseLessons/CourseLessons.tsx
+++ dun/frontend/src/pages/Course/CourseLessons/CourseLessons.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { DeleteOutlined, EditOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, Col, Row, Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { Lesson } from '~/api';
+import { CourseLesson } from '~/pages/Course/CourseLesson';
+import { CourseLessonsPlaceholder } from '~/pages/Course/CourseLessons/CourseLessonsPlaceholder';
+
+const { Title } = Typography;
 
 export type CourseLessonsProps = {
   lessons: Array<Lesson> | undefined;
@@ -11,18 +17,37 @@ export function CourseLessons({ lessons, className }: CourseLessonsProps) {
   const { t } = useTranslation();
 
   return (
-    <div className={className}>
-      <div>{t('lessons')}</div>
+    <Col className={className} span={24}>
+      <Row justify="space-between">
+        <Col>
+          <Title level={3}>{t('lessons.title')}</Title>
+        </Col>
+        <Col>
+          <Button type="text" icon={<PlusOutlined />} color="primary">
+            {t('lessons.new_lesson')}
+          </Button>
+        </Col>
+      </Row>
 
-      {lessons ? (
-        <>
-          {lessons.map((lesson) => (
-            <div key={lesson.id}>{lesson.title}</div>
-          ))}
-        </>
-      ) : (
-        <div>Empty todo</div>
-      )}
-    </div>
+      {lessons &&
+        (lessons.length ? (
+          <>
+            {lessons.map((lesson) => (
+              <CourseLesson key={lesson.id} lesson={lesson}>
+                <Button type="text" shape="circle">
+                  <EditOutlined />
+                </Button>
+                <Button type="text" shape="circle">
+                  <DeleteOutlined />
+                </Button>
+              </CourseLesson>
+            ))}
+          </>
+        ) : (
+          <CourseLessonsPlaceholder>
+            <Button type="primary">{t('lessons.new_lesson')}</Button>
+          </CourseLessonsPlaceholder>
+        ))}
+    </Col>
   );
 }
