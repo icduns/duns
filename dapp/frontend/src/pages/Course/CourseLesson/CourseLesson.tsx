c35806ae22e4991b66f1c9e627f656a3ad7d1c30
v.lipko <v.lipko@bacup.ru>
2022-09-17 15:45:21 +0700

    feat(front): add reordering lessons

--- dapp/frontend/src/pages/Course/CourseLesson/CourseLesson.tsx
+++ dapp/frontend/src/pages/Course/CourseLesson/CourseLesson.tsx
@@ -1,18 +1,109 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useRef } from 'react';
 import { Col, Row, Space, Typography } from 'antd';
-import { Lesson } from '../../../../../../declarations/dun_backend/dun_backend.did';
+import classNames from 'classnames';
+import type { Identifier, XYCoord } from 'dnd-core';
+import { DropTargetHookSpec, useDrag, useDrop } from 'react-dnd';
+import { Lesson } from '~/api';
 import styles from './CourseLesson.module.less';
 
 const { Title } = Typography;
-type CourseLessonProps = PropsWithChildren<{ lesson: Lesson }>;
+const DND_TYPE = 'LESSON';
+
+export type CourseLessonProps = PropsWithChildren<{
+  lesson: Lesson;
+  index: number;
+  moveLesson: (dragIndex: number, hoverIndex: number) => void;
+}>;
+type DragItem = { index: number };
+type DragCollectedProps = { isDragging: boolean };
+type DropCollectedProps = { handlerId: Identifier | null };
 
 export function CourseLesson(props: CourseLessonProps) {
-  const { lesson, children } = props;
+  const { lesson, children, moveLesson, index } = props;
+  const ref = useRef<HTMLDivElement>(null);
+  const title = `${index + 1}. ${lesson.title}`;
+
+  const [{ isDragging }, drag] = useDrag<DragItem, void, DragCollectedProps>({
+    type: DND_TYPE,
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+    item: () => ({ index }),
+  });
+
+  const dropFn: DropTargetHookSpec<
+    DragItem,
+    void,
+    DropCollectedProps
+  >['drop'] = (item, monitor) => {
+    if (!ref.current) {
+      return;
+    }
+
+    const dragIndex = item.index;
+    const dropIndex = index;
+
+    // Don't replace items with themselves
+    if (dragIndex === dropIndex) {
+      return;
+    }
+
+    // Determine rectangle on screen
+    const hoverBoundingRect = ref.current?.getBoundingClientRect();
+
+    // Get vertical middle
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+    // Determine mouse position
+    const clientOffset = monitor.getClientOffset();
+
+    // Get pixels to the top
+    const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top;
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+    if (dragIndex < dropIndex && hoverClientY < hoverMiddleY) {
+      return;
+    }
+
+    // Dragging upwards
+    if (dragIndex > dropIndex && hoverClientY > hoverMiddleY) {
+      return;
+    }
+
+    // Time to actually perform the action
+    moveLesson(dragIndex, dropIndex);
+  };
+
+  const [{ handlerId }, drop] = useDrop<DragItem, void, DropCollectedProps>({
+    accept: DND_TYPE,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    drop: dropFn,
+  });
+
+  drop(drag(ref));
+
   return (
-    <Row justify="space-between" align="middle" className={styles.CourseLesson}>
+    <Row
+      ref={ref}
+      justify="space-between"
+      align="middle"
+      className={classNames(
+        styles.courseLesson,
+        isDragging && styles.courseLesson__dragging,
+      )}
+      data-handler-id={handlerId}
+    >
       <Col>
         <Title ellipsis level={5} style={{ margin: 0 }}>
-          {lesson.title}
+          {title}
         </Title>
       </Col>
       <Col>
--- dapp/frontend/src/pages/Course/CourseLessons/CourseLessons.tsx
+++ dapp/frontend/src/pages/Course/CourseLessons/CourseLessons.tsx
@@ -1,20 +1,22 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   BuildOutlined,
   DeleteOutlined,
   EditOutlined,
   PlusOutlined,
 } from '@ant-design/icons';
-import { Button, Col, Modal, Row, Typography } from 'antd';
+import { Button, Col, Modal, Row, Spin, Typography } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import { call, Lesson } from '~/api';
-import { CourseLesson } from '~/pages/Course/CourseLesson';
+import { CourseLesson, CourseLessonProps } from '~/pages/Course/CourseLesson';
 import {
   CourseLessonModal,
   CourseLessonModalProps,
 } from '~/pages/Course/CourseLessonModal';
 import { CourseLessonsPlaceholder } from '~/pages/Course/CourseLessons/CourseLessonsPlaceholder';
+import { swapArrayItems } from '~/utils/swapArrayItems';
+import styles from './CourseLessons.module.less';
 
 const { confirm } = Modal;
 const { Title } = Typography;
@@ -24,16 +26,16 @@ export type CourseLessonsProps = {
   lessons: Array<Lesson> | undefined;
   onAction: () => void;
   courseId: Lesson['courseId'];
-  className?: string;
 };
 
 export function CourseLessons(props: CourseLessonsProps) {
-  const { lessons, className, onAction, courseId } = props;
+  const { lessons, onAction, courseId } = props;
   const { t } = useTranslation();
   const [modalData, setModalData] = useState<ModalData>({
     type: 'create',
     visible: false,
   });
+  const [loading, setLoading] = useState(false);
 
   const handleCreate = useCallback(
     () => setModalData({ visible: true, type: 'create' }),
@@ -67,9 +69,34 @@ export function CourseLessons(props: CourseLessonsProps) {
     [onAction, t],
   );
 
+  const handleMoveLesson: CourseLessonProps['moveLesson'] = useCallback(
+    (dragIndex, dropIndex) => {
+      if (!lessons) {
+        return;
+      }
+
+      setLoading(true);
+      const newOrder = swapArrayItems(
+        lessons.map(({ id }) => id),
+        dragIndex,
+        dropIndex,
+      );
+      call('orderLessons', courseId, newOrder)
+        .then(() => onAction())
+        .catch(() => setLoading(false));
+    },
+    [courseId, lessons, onAction],
+  );
+
+  useEffect(() => {
+    if (lessons) {
+      setLoading(false);
+    }
+  }, [lessons]);
+
   return (
-    <>
-      <Col className={className} span={24}>
+    <Spin spinning={loading} wrapperClassName={styles.courseLessons_spin}>
+      <Col span={24}>
         <Row justify="space-between">
           <Col>
             <Title level={3}>{t('lessons.title')}</Title>
@@ -91,8 +118,13 @@ export function CourseLessons(props: CourseLessonsProps) {
         {lessons &&
           (lessons.length ? (
             <>
-              {lessons.map((lesson) => (
-                <CourseLesson key={lesson.id} lesson={lesson}>
+              {lessons.map((lesson, index) => (
+                <CourseLesson
+                  key={lesson.id}
+                  index={index}
+                  lesson={lesson}
+                  moveLesson={handleMoveLesson}
+                >
                   <Button
                     type="text"
                     shape="circle"
@@ -134,6 +166,6 @@ export function CourseLessons(props: CourseLessonsProps) {
         }
         onSubmit={handleSubmit}
       />
-    </>
+    </Spin>
   );
 }
